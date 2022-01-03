a10437c913d18d723bcdcf2fc3e10fdc504e7e28
darian93 <darianreynaldi@gmail.com>
2022-01-03 13:20:46 +0700

    init-commit-2

--- src/backend/database/index.js
+++ src/backend/database/index.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:'192.168.0.10',
+    host:'192.168.1.4',
     user:'root',
     password: 'password',
     database: 'db_telaga',
